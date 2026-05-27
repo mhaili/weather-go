3c42f670af1aa470fb3f02e0b49617db80515e7b
mhaili <73223991+mhaili@users.noreply.github.com>
2026-05-27 14:19:12 +0200

    feat(B): modèle interne

--- model.go
+++ model.go
@@ -1,1 +1,27 @@
 package weather_go
+
+// station météo
+type Station struct {
+	Country      string
+	Altitude     int
+	Location     Coordinates
+	DeviceModel  string
+	Observations []Observation
+}
+
+type Coordinates struct {
+	Latitude  float64
+	Longitude float64
+}
+
+// une mesure à un t donné
+type Observation struct {
+	Temperature float64
+	Conditions  string
+	Wind        Wind
+	Note        string
+}
+type Wind struct {
+	Speed        float64
+	DirectionDeg int
+}
